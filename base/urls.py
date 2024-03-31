6831aeeea4edb366cbfc31292e05cd4b45a22181
GideonMissoy <giddykips2030@gmail.com>
2024-03-31 23:47:25 +0300

    Making progress.

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import customLogin, homeIndex
+from .views import HomeTemplate, AboutTemplate, SuppliesTemplate, ContactTemplate,  ListingsTemplate, ContractorsTemplate, LogTemplate, ProfileTemplate
 
 urlpatterns = [
-    path('', homeIndex, name='index'),
-    path('login/', customLogin, name='login')
+    path('', HomeTemplate.as_view(), name='index'),
+    path('about/', AboutTemplate.as_view(), name='about'),
+    path('buy-supplies/', SuppliesTemplate.as_view(), name='buy-supplies'),
+    path('contact/', ContactTemplate.as_view(), name='contact'),
+    path('contracts/', ListingsTemplate.as_view(), name='contracts'),
+    path('contractors/', ContractorsTemplate.as_view(), name='contractors'),
+    path('login/', LogTemplate.as_view(), name='login'),
+    path('profile/', ProfileTemplate.as_view(), name='profile'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView
 from django.http import HttpResponse
 
 def homeIndex(request):
     return render(request, 'base/index.html')
 
-def customLogin(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-    return render(request, 'base/login.html')
+# def customLogin(request):
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')
+#     return render(request, 'base/login.html')
+
+class HomeTemplate(TemplateView):
+    template_name = 'index.html'
+
+class AboutTemplate(TemplateView):
+    template_name = 'about.html'
+
+class SuppliesTemplate(TemplateView):
+    template_name = 'buy_supplies.html'
+
+class ContactTemplate(TemplateView):
+    template_name = 'contact.html'
+
+class ListingsTemplate(TemplateView):
+    template_name = 'contracts.html'
+
+class ContractorsTemplate(TemplateView):
+    template_name = 'contractors.html'
+
+class LogTemplate(TemplateView):
+    template_name = 'login.html'
+
+class ProfileTemplate(TemplateView):
+    template_name = 'profile.html'
