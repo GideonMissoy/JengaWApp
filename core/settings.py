65da2e0d13340cc07f62495a2c8ad460ff8d75e3
GideonMissoy <giddykips2030@gmail.com>
2024-04-10 23:44:59 +0300

    Updating the Project ListView.

--- core/settings.py
+++ core/settings.py
@@ -116,3 +116,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'login'
