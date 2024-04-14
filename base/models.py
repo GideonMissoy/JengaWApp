0d0b5dec2d3dd7ab3892be672bfaa0ddaea9cd17
GideonMissoy <giddykips2030@gmail.com>
2024-04-14 23:53:57 +0300

    Fixed the status options.

--- base/models.py
+++ base/models.py
@@ -7,7 +7,7 @@ class Project(models.Model):
     STATUS_CHOICES = (
         ('Open', 'Open'),
         ('In Progress', 'In Progress'),
-        ('Completed', '(Completed)'),
+        ('Completed', 'Completed'),
     )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -15,7 +15,7 @@ class Project(models.Model):
     budget = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     location = models.CharField(max_length=100, null=True, blank=True)
     description  = models.TextField(null=True, blank=True)
-    status = models.BooleanField(default=False)
+    status = models.CharField(choices=STATUS_CHOICES, default='Open', max_length=20)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
